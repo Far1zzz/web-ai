9b02c0b4a694f62cf93156ad5812265ffa7d7f4d
farizz <fairizwalker@gmail.com>
2023-05-08 19:49:39 +0700

    demmm

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,7 +5,8 @@ import React, { useContext, useEffect, useState } from "react";
 import { LayoutContext } from "../layout/context/layoutcontext";
 import { InputText } from "primereact/inputtext";
 import { FilterMatchMode, FilterOperator } from "primereact/api";
-import data from "../public/demo/data/daftar-penyakit.json";
+import { MultiSelect } from "primereact/multiselect";
+import { Button } from "primereact/button";
 
 const Dashboard = () => {
   const [listPenyakit, setListPenyakit] = useState([]);
@@ -149,17 +150,147 @@ const Dashboard = () => {
   const handleFilter = (event) => {
     event.preventDefault();
 
-    const penyakitTerfilter = data.data.filter((res) => {
+    const penyakitTerfilter = listPenyakit.filter((res) => {
       return res.gejala_penyakit.some((gejala) =>
         inputGejala.includes(gejala.toString())
       );
     });
 
+    penyakitTerfilter.sort((a, b) => {
+      const aMatchCount = a.gejala_penyakit.filter((gejala) =>
+        inputGejala.includes(gejala.toString())
+      ).length;
+
+      const bMatchCount = b.gejala_penyakit.filter((gejala) =>
+        inputGejala.includes(gejala.toString())
+      ).length;
+
+      return bMatchCount - aMatchCount;
+    });
+
     setHasilFilter(penyakitTerfilter);
   };
 
+  const [selectedGejala, setSelectedGejala] = useState([]);
+  const [filtered, setFiltered] = useState([]);
+  const gejalaOptions = [
+    { value: "1", label: "Demam" },
+    { value: "2", label: "Sakit Kepala" },
+    { value: "3", label: "Nyeri Saat Bicara Atau Menelan" },
+    { value: "4", label: "Batuk" },
+    { value: "5", label: "Hidung Tersumbat" },
+    { value: "6", label: "Nyeri Telinga" },
+    { value: "7", label: "Nyeri Tenggorokan" },
+    { value: "8", label: "Hidung Meler" },
+    { value: "9", label: "Letih Dan Lesu" },
+    { value: "10", label: "Mual Dan Muntah" },
+    { value: "11", label: "Selaput Lendir Merah Dan Bengkak" },
+    { value: "12", label: "Ada Benjolan Di Leher" },
+    { value: "13", label: "Nyeri Leher" },
+    { value: "14", label: "Pembengkakan Kelenjar Getah Bening" },
+    { value: "15", label: "Pendarahan Hidung" },
+    { value: "16", label: "Suara Serak" },
+    { value: "17", label: "Bola Mata Bergetar Tanpa Sadar" },
+    { value: "18", label: "Dahi sakit" },
+    { value: "19", label: "Leher Bengkak" },
+    { value: "20", label: "tuli" },
+    { value: "21", label: "Ada Yang Tumbuh Di mulut" },
+    { value: "22", label: "Air Liur Menetes" },
+    { value: "23", label: "Berat Badan Turun" },
+    { value: "24", label: "Bunyi Nafas Abnormal" },
+    { value: "25", label: "Insfeksi Sinus" },
+    { value: "26", label: "Nyeri Antara Mata" },
+    { value: "27", label: "Nyeri Pinggir Hidung" },
+    { value: "28", label: "Nyeri Pipi Di Bawah Mata" },
+    { value: "29", label: "Nyeri Wajah" },
+    { value: "30", label: "Perubahan Kulit" },
+    { value: "31", label: "Perubahan Suara" },
+    { value: "32", label: "Radang Gendang Telinga" },
+    { value: "33", label: "Sakit Gigi" },
+    { value: "34", label: "Serangan Vertigo" },
+    { value: "35", label: "Telinga Berdenging" },
+    { value: "36", label: "Telinga Terasa Penuh" },
+    { value: "37", label: "Tenggorokan Gatal" },
+    { value: "38", label: "Tubuh Tak Seimbang" },
+  ];
+
+  const handleFiltered = (event) => {
+    event.preventDefault();
+
+    const penyakitTerfilter = listPenyakit.filter((res) => {
+      return res.gejala_penyakit.some((gejala) =>
+        selectedGejala.includes(gejala.toString())
+      );
+    });
+
+    penyakitTerfilter.sort((a, b) => {
+      const aMatchCount = a.gejala_penyakit.filter((gejala) =>
+        selectedGejala.includes(gejala.toString())
+      ).length;
+
+      const bMatchCount = b.gejala_penyakit.filter((gejala) =>
+        selectedGejala.includes(gejala.toString())
+      ).length;
+
+      return bMatchCount - aMatchCount;
+    });
+
+    setFiltered(penyakitTerfilter);
+  };
+
+  const handleSelectChange = (event) => {
+    setSelectedGejala(event.value);
+  };
+  const renderHeaderCek = () => {
+    return (
+      <form
+        className="flex justify-content-start gap-2"
+        onSubmit={handleFiltered}
+      >
+        <MultiSelect
+          value={selectedGejala}
+          options={gejalaOptions}
+          onChange={handleSelectChange}
+          placeholder="Pilih Gejala"
+          filter
+          filterPlaceholder="Cari Gejala"
+        />
+        <Button
+          type="submit"
+          icon="pi pi-search"
+          severity="success"
+          aria-label="Search"
+        />
+      </form>
+    );
+  };
+
+  const headerCek = renderHeaderCek();
+
+  console.log(selectedGejala);
+  console.log(inputGejala);
   return (
     <>
+      <div className="card">
+        <DataTable
+          value={filtered}
+          paginator
+          rows={10}
+          dataKey="id"
+          filters={filterPenyakit}
+          loading={loading}
+          rowsPerPageOptions={[5, 10, 25, 50]}
+          globalFilterFields={["gejala_penyakit"]}
+          header={headerCek}
+          emptyMessage="Gejala Belum Di Input"
+        >
+          <Column
+            field="nama_penyakit"
+            header="Nama Penyakit"
+            style={{ minWidth: "12rem" }}
+          />
+        </DataTable>
+      </div>
       <div className="card">
         <span className="text-center">
           <h3>Page Cek Gejala</h3>
@@ -206,10 +337,10 @@ const Dashboard = () => {
         </form>
         <br />
         {hasilFilter.length > 0 ? (
-          hasilFilter.map((penyakit) => (
-            <div key={penyakit.id}>
-              <p>Nama Penyakit: {penyakit.nama_penyakit}</p>
-              <p>Gejala: {penyakit.gejala_penyakit.join(", ")}</p>
+          hasilFilter.map((res) => (
+            <div key={res.id}>
+              <p>Nama Penyakit: {res.nama_penyakit}</p>
+              <p>Gejala: {res.gejala_penyakit.join(", ")}</p>
               <hr />
             </div>
           ))
